e5c79de2aa005a2f5c7dd8acda9d0d089f99e517
amolpawar24 <amolpawar.me@gmail.com>
2026-09-02 18:55:19 +0530

    Add initial JavaScript features and engine details

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+ //! There are Some features of JavaScript
+
+ //* 1.Scripting Language
+ //* 2.Interpreted Language
+ //* 3.Light-Weight language
+ //* 4.WeaklyTyped Language
+ //* 5.Object Based Language
+
+
+// ! We cannot perform the Function Overloading in JavaScript
+//* Because JavaScript has Default Datatype called as "Undefined"  
+
+
+//! In JavaScript Entire Memory Management is doing the JsEngine
+//todo : we do not worry about the Memory Management
+
+//! Different Js Engines for the Different Bowers 
+//* 1.Chrome has V8
+//* 2.Mozilla Has SpiderMonkey
+//* 3.Edge has Chakara
+//* 4.Safari has JsCore    
