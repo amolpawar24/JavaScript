ecb59b5d4b685c40e2288e093d94fd38f1233024
amolpawar24 <amolpawar.me@gmail.com>
2026-09-05 17:50:08 +0530

    Add Variables.js to illustrate variable declaration, initialization, re-initialization, and re-declaration in JavaScript

---
+++ VARIABLES/Variables.js
@@ -0,0 +1,28 @@
+//! Variables
+//todo: Variabales are containers for storing information  
+//todo: Variables is a block of container used to store the value  
+
+//! 1.Variables Declaration
+//* Synax:
+//? variableType variableName 
+//todo: var/let/const carName   
+//todo: var carName;
+
+//! 2.Variables Initialization
+//* Syntax:
+//? var carName; // Declaration
+//? carName = "Volvo"; //Initialization   
+
+//! 3.Variable Declaration With Initialization
+//* Syntax:
+//? var carName = "Volvo"; //Declaration With Initialization
+
+//! 4.Re-Initialization
+//* Syntax:
+//? var carName = "Volvo"; // Declaration With Initialization
+//? carName = "BMW"; //Re-Initialization
+
+//! 5.Re-Declaration
+//* Syntax:
+//? var carName = "Volvo"; //Declaration With Initialization
+//? var carName = "BMW";  //Re-Declaration
