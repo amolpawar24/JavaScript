23b3f5a83e419d5da39a1c644586beb387cb195b
amolpawar24 <amolpawar.me@gmail.com>
2026-09-06 14:17:22 +0530

    Add External.js to demonstrate basic JavaScript functionality with functions and date handling

---
+++ JAVASCRIPT TYPES/EXTERNAL/External.js
@@ -0,0 +1,9 @@
+//? 1.First Script  -->
+    function myFunction() {
+      document.getElementById("demo").innerHTML = "Paragraph changed.";
+    }
+
+//? 2.Second Script-->
+
+    let d = new Date();
+    document.write("Today's date is " + d);
