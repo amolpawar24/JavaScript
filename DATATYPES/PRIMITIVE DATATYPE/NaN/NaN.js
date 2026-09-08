39c5cfee2793c0b66267a29dc69b6d33c27e1486
amolpawar24 <amolpawar.me@gmail.com>
2026-09-08 17:02:42 +0530

    Add NaN.js to explain NaN in JavaScript and provide examples

---
+++ DATATYPES/PRIMITIVE DATATYPE/NaN/NaN.js
@@ -0,0 +1,11 @@
+//! NaN
+//* NaN Stands for Not a Number.
+//* NaN is a Special value in JavaScript that represents the result of an invalid mathematical operation or an undefined numerical result.
+//* NaN is Not Seperate DataType, it is a part of Number DataType.
+//todo: It appears when a numeric calculation fails or when a non-numeric value is used in a mathematical operation.
+
+//* Example of NaN
+console.log(0 / 0); // Output: NaN
+console.log(Math.sqrt(-1)); // Output: NaN
+console.log(parseInt("Hello")); // Output: NaN 
+console.log();
