98b91dbadfe58c0f9f1c8043f01dcc70f5080ab7
amolpawar24 <amolpawar.me@gmail.com>
2026-09-08 17:01:00 +0530

    Add DataTypes.js to outline primitive and non-primitive data types in JavaScript

---
+++ DATATYPES/DataTypes.js
@@ -0,0 +1,17 @@
+// ! There are Two types of DataType in JavaScript
+
+//* 1.Primitive DataType
+//todo 1.Number
+//todo 2.String
+//todo 3.Boolean  
+//todo 4.Null
+//todo 5.Undefined 
+//todo 6.Symbol
+//todo 7.BigInt
+
+//* 2.Non-Primitive DataType
+//todo 1.Object 
+//todo 2.Array
+//todo 3.Function
+//todo 4.Date
+//todo 5.RegExp
