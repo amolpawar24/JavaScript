58d3a1ebd0ad1ae0f1139f28f2dc30b0a8fba314
amolpawar24 <amolpawar.me@gmail.com>
2026-09-09 18:34:50 +0530

    Add Backtick.js to demonstrate template literals and variable interpolation in JavaScript

---
+++ BACKTICK/Backtick``.js
@@ -0,0 +1,12 @@
+//* let name = "Ajay";
+//* let age = 24;
+//* let proff = "Engineer" 
+//* console.log('My Name is"+name+"My Age is" +age+ "My Proffession is" +proff) 
+
+//! To avoid collisions we use the following :
+
+//? ` ` backtick 
+//* let name = "Ajay";
+//* let age = 24;
+//* let proff = "Engineer"   
+//* console.log('My Name is ${name} My Age is ${age} My Proffession is ${proff}`) 
