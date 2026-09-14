8136f58c3db59729e18380be350104b453280805
amolpawar24 <amolpawar.me@gmail.com>
2026-09-14 21:19:35 +0530

    Add String.js to demonstrate string properties and creation methods in JavaScript

---
+++ DATATYPES/PRIMITIVE DATATYPE/STRING/String.js
@@ -0,0 +1,25 @@
+//! STRING
+
+//! STRING ARE IMMUTABLE IN NATURE 
+
+//todo : The sequence of character is called as String
+
+//* We write String in 
+//? 1." String "
+//? 2.' String '
+//? 3.` String `
+
+//! WE CAN WRITE ALSO
+//* 1." ' String ' " 
+//* 2." ` String ` "
+
+//! 1. " " " " It is Not Allowed because same Sign is Dominated to each other
+//! 2. ` ` ` ` It is Not Allowed because same Sign is Dominated to each other
+//! 3. ' ' ' ' It is Not Allowed because same Sign is Dominated to each other
+
+//! " " String " "  it is not allowed
+
+
+//* We can create the String using Two Ways
+//* 1.Using Literals
+//* 2.Using New Keyword   
