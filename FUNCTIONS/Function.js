9f7d4f3d39f7661c428962ca5a218f554de3857f
amolpawar24 <amolpawar.me@gmail.com>
2026-09-14 21:20:46 +0530

    Add Function.js to document different types of functions in JavaScript

---
+++ FUNCTIONS/Function.js
@@ -0,0 +1,11 @@
+//! Function : Function is a set of instruction to perform the set of instructions
+//* Function have some functionality to perform some Action
+//* Function have some instruction according to that instruction it performs some Action 
+
+//! There are 5 Types of Function in JavaScript
+
+//* 1.Function Declaration 
+//* 2.Function Expression
+//* 3.Anonymous Function(No Name)
+//* 4.Arrow Function ====> ES06 Function
+//* 5.IIFE Function(Immediate Invocation Function) ====> ES05 Function
